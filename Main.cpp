dc125cfc75ec0099bce956a43b7d1183aa6d0b34
NematekGSD <nematekgsd@yahoo.com>
2025-01-13 10:28:02 -0500

    Added Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int MyFavouriteNumber{ 0 };
+
+	cout << "Enter your favourite number between 1 and 100: ";
+	cin >> MyFavouriteNumber;
+	cout << "Amazing!!!  That's my favourite number too!\n\n";
+	cout << "No really, " << MyFavouriteNumber << " is my favourite number!\n\n";
+
+	return 0;
+}
